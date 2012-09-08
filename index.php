27d99f7847e985c9ef26ba0f6e380dd877508a65
Espeon <espeon@minet.net>
2012-09-09 00:38:03 +0200

    Déploiement de la carte sur tout l'écran
    Le navigateur est le screen du jeu
    Test de build des décors en HTML/CSS

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+	<!-- Title -->
+	<title>MiNET [07/12/12] : J-XX</title>
+	
 	<!-- Meta -->
 	<meta name="description" content="Soirée MiNET 2012... Coming Soon !" />
 	<meta name="keywords" content="MiNET, soirée, hack, Télécom, Soirée MiNET 2012, TMSP, TEM, TSP, Télécom SudParis, Télécom École de Management" />
 	<meta name="author" content="Espeon" />
 	<meta charset="UTF-8" />
 	
-	<!-- Title -->
-	<title>MiNET [07/12/12] : J-XX</title>
-	
 	<!-- CSS -->
 	<link rel="stylesheet" type="text/css" href="css/reset.css" />
 	<link rel="stylesheet" type="text/css" href="css/main.css" />
 	
-	<!--[if lt IE 9]><script type="text/javascript" src="js/excanvas.js"></script><![endif]-->
+	<!--[if lt IE 9]><![endif]-->
 </head>
 <body>
 	<!-- Menu de navigation -->
@@ -27,7 +27,23 @@
 	<div id="notification"></div>
 	
 	<!-- Contenu du jeu -->
-	<div id="wrapper"></div>
+	<div id="wrapper">
+		<!-- Montagnes du Nord -->
+		<div id="northMountain" class="mountain">
+			<div class="corner"></div>
+		</div>
+		
+		<!-- Ouest-->
+		
+		
+		<!-- Montagnes de l'Ouest -->
+		<div id="westMountain" class="mountain">
+			<div class="corner"></div>
+		</div>
+		
+		<!-- Lac du Sud -->
+		<div id="southSea" class="water"></div>
+	</div>
 	
 	<!-- JS -->
 	<script type="text/javascript" src="js/jquery-1.7.js"></script>
