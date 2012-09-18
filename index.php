86c3a2f84e254d328cc4270b61d89634fe0cb510
Espeon <espeon@minet.net>
2012-09-18 22:12:16 +0200

    Nouvelle police pour navbar

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 	<link rel="stylesheet" type="text/css" href="css/reset.css" />
 	<link rel="stylesheet" type="text/css" href="css/main.css" />
 	<link rel="stylesheet" type="text/css" href="css/map.css" />
+	<link href='http://fonts.googleapis.com/css?family=Press+Start+2P' rel='stylesheet' type='text/css'>
 	
 	<!--[if lt IE 9]><![endif]-->
 </head>
